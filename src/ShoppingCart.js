cd8bec7efd14befaa405aba89d60d275d86bd523
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-08 15:33:59 +0200

    Completed ShoppingCart class, passes all tests

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,16 +1,22 @@
 module.exports = class ShoppingCart {
     constructor() {
         this.cartContents = []
-        this.item = {}
+    }
+    addItem(itemName, quantity, price) {
+        const item = {name: itemName, quantity: quantity, pricePerUnit: price }
+        return this.cartContents.push(item)
     }
     getItems() {
         return this.cartContents
     }
-    addItem(itemName, quantity, price) {}
-    clear(){}
+    clear() {
+        this.cartContents.splice(0, this.cartContents.length)
+    }
+    total() {
+        return this.cartContents.reduce((totalPrice, currentItem) => {
+                return totalPrice + (currentItem.quantity * currentItem.pricePerUnit)
+        }, 0)
+    }
+
 }
-
-
-
-console.log(module.exports)
 
